7af5720d07e40dc01ce161dc18a7ec54dd5f789c
Koichi Shiraishi <zchee.io@gmail.com>
2021-03-10 02:22:44 +0900

    doc: add package top level synopsis

---
+++ doc.go
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: Copyright 2021 The go-capstone Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Package capstone provides the Go binding for the Capstone disassembly/disassembler framework.
+package capstone
